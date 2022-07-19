2b562b13c0fe041d9acbf92c2bc11a2e186cdec8
faizasidd <faiza.siddiqi1@gmail.com>
2022-07-19 17:51:36 -0400

    Created a new warehouse

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
-// const warehousesRoute = require('./routes/warehouses.js');
+const warehousesRoute = require('./routes/warehouses.js');
 // const inventoriesRoute = require('./routes/inventories.js');
 
 
@@ -11,7 +11,7 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cors());
 
-// app.use('/warehouses', warehousesRoute);
+ app.use('/warehouses', warehousesRoute);
 // app.use('/inventories', inventoriesRoute);
 
 
