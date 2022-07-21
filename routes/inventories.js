b19b7bae55fa2d2b35fbaeda9e8ab9888dfe6662
unknown <yikfungseant@gmail.com>
2022-07-21 13:19:47 -0400

    second commit for delete inventory item

--- routes/inventories.js
+++ routes/inventories.js
@@ -5,12 +5,10 @@ let inventories = require('../data/inventories.json')
 
 //deleting inventory items 
 router.delete('/inventory/:inventoryId', (req, res) => {
-    //taking inventoryId as a param
+    
     const {inventoryId} = req.params
-    //finding the specific index of the inventory in the json
     const requestInventory = inventories.findIndex(inventory => inventory.id === inventoryId)
 
-    //applying the above index to the json as 'inventory' to use as a body
     const inventory = inventories[requestInventory];
 
     inventories.splice(requestInventory, 1)
