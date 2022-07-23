eb1196c6e61ab61a4be591b5c2f356a2b2c561d5
John Williams <jw@mlmpipa.org>
2022-07-23 13:41:17 -0400

    just commiting a bug fix before a pull

--- routes/inventories.js
+++ routes/inventories.js
@@ -9,145 +9,145 @@ const path = require("path");
 router.delete("/:inventoryId", (req, res) => {
   const { inventoryId } = req.params;
   
-// GET full details on all inventory, array of objects
-router.get('/', (req, res) => {
-  console.log('Here is a list of the inventory')
+  // GET full details on all inventory, array of objects
+  router.get('/', (req, res) => {
+    console.log('Here is a list of the inventory')
 
-router.get('/', (req, res) => {
-  console.log('get inventories')
-  res.status(200).json(inventories)
-})
+    router.get('/', (req, res) => {
+      console.log('get inventories')
+      res.status(200).json(inventories)
+    })
 
-//deleting inventory items 
-router.delete('/:inventoryId', (req, res) => {
+    //deleting inventory items 
+    router.delete('/:inventoryId', (req, res) => {
     
-    const {inventoryId} = req.params
+      const { inventoryId } = req.params
    
-    const requestInventory = inventories.findIndex(inventory => inventory.id === inventoryId)
+      const requestInventory = inventories.findIndex(inventory => inventory.id === inventoryId)
    
+      const inventory = inventories[requestInventory];
+
+      inventories.splice(requestInventory, 1)
+
+      const dataObjectAsString = JSON.stringify(inventories, null, 2);
+      fs.writeFile(__dirname + '/../data/inventories.json', dataObjectAsString, (err) => {
+        console.log(err)
+      })
+      res.status(200).json(inventory)
+    });
+
+    const requestInventory = inventories.findIndex(
+      (inventory) => inventory.id === inventoryId
+    );
+
     const inventory = inventories[requestInventory];
 
-    inventories.splice(requestInventory, 1)
+    inventories.splice(requestInventory, 1);
 
     const dataObjectAsString = JSON.stringify(inventories, null, 2);
-    fs.writeFile(__dirname + '/../data/inventories.json', dataObjectAsString, (err) => {
-        console.log(err)
-    })
-    res.status(200).json(inventory)
-});
-
-  const requestInventory = inventories.findIndex(
-    (inventory) => inventory.id === inventoryId
-  );
-
-  const inventory = inventories[requestInventory];
-
-  inventories.splice(requestInventory, 1);
-
-  const dataObjectAsString = JSON.stringify(inventories, null, 2);
-  fs.writeFile(__dirname + "/../data/inventories.json", dataObjectAsString, (err) => {
-    console.log(err);
-  });
-  res.status(200).json(inventory);
-});
-
-// GET a Single Inventory Item
-
-router.get("/:inventoryId", (req, res) => {
-  const id = req.params.inventoryId;
-  const selectedInventory = inventories.filter(
-    (inventory) => inventory.id === id
-  );
-
-  if (selectedInventory) {
-    res.status(200).send(selectedInventory);
-  } else {
-    res.status(400).json(`Inventory with id: ${id} does not exist`);
-  }
-});
-
-// POST/CREATE new Inventory Item
-
-router.post("/", (req, res) => {
-  //   res.send("Hello this is a successful post request!");
-  const warehouses = require("../data/warehouses.json");
-  const inventories = require("../data/inventories.json");
-
-  const {
-    warehouseID,
-    warehouseName,
-    itemName,
-    description,
-    category,
-    status,
-    quantity,
-  } = req.body;
-  id = uuidv4();
-  const findWarehouse = (warehouseToFind) => {
-    const matchingWarehouse = warehouses.find(
-      (warehouse) => warehouse.name === warehouseToFind
-    );
-    if (matchingWarehouse) {
-      return matchingWarehouse.id;
-    }
-    console.log("no matching warehouse found");
-    return "";
-  };
-
-  const newInventoryData = {
-    id,
-    warehouseID: findWarehouse(warehouseName),
-    warehouseName,
-    itemName,
-    description,
-    category,
-    status,
-    quantity,
-  };
-  inventories.push(newInventoryData);
-  console.log(newInventoryData);
-
-  const newInventoryJSON = JSON.stringify(inventories, null, 2);
-  fs.writeFile("./data/inventories.json", newInventoryJSON, "utf8", (err) => {
-    if (err) {
+    fs.writeFile(__dirname + "/../data/inventories.json", dataObjectAsString, (err) => {
       console.log(err);
+    });
+    res.status(200).json(inventory);
+  });
+
+  // GET a Single Inventory Item
+
+  router.get("/:inventoryId", (req, res) => {
+    const id = req.params.inventoryId;
+    const selectedInventory = inventories.filter(
+      (inventory) => inventory.id === id
+    );
+
+    if (selectedInventory) {
+      res.status(200).send(selectedInventory);
     } else {
-      res.send(200);
+      res.status(400).json(`Inventory with id: ${id} does not exist`);
     }
   });
-});
 
-// PUT/PATCH/EDIT Inventory Item
+  // POST/CREATE new Inventory Item
 
-router.put("/:inventoryId", (req, res) => {
-  const inventories = require("../data/inventories.json");
-  const inventoryId = req.params.inventoryId;
-  const matchingInventory = inventories.find(
-    (inventory) => inventory.id === inventoryId
-  );
+  router.post("/", (req, res) => {
+    //   res.send("Hello this is a successful post request!");
+    const warehouses = require("../data/warehouses.json");
+    const inventories = require("../data/inventories.json");
 
-  if (matchingInventory) {
-    matchingInventory.warehouseName = req.body.warehouseName;
-    matchingInventory.itemName = req.body.itemName;
-    matchingInventory.description = req.body.description;
-    matchingInventory.category = req.body.category;
-    matchingInventory.status = req.body.status;
-    matchingInventory.quantity = req.body.quantity;
-  }
+    const {
+      warehouseID,
+      warehouseName,
+      itemName,
+      description,
+      category,
+      status,
+      quantity,
+    } = req.body;
+    id = uuidv4();
+    const findWarehouse = (warehouseToFind) => {
+      const matchingWarehouse = warehouses.find(
+        (warehouse) => warehouse.name === warehouseToFind
+      );
+      if (matchingWarehouse) {
+        return matchingWarehouse.id;
+      }
+      console.log("no matching warehouse found");
+      return "";
+    };
 
-  fs.writeFile(
-    "./data/inventories.json",
-    JSON.stringify(inventories, null, 2),
-    "utf8",
-    (err) => {
+    const newInventoryData = {
+      id,
+      warehouseID: findWarehouse(warehouseName),
+      warehouseName,
+      itemName,
+      description,
+      category,
+      status,
+      quantity,
+    };
+    inventories.push(newInventoryData);
+    console.log(newInventoryData);
+
+    const newInventoryJSON = JSON.stringify(inventories, null, 2);
+    fs.writeFile("./data/inventories.json", newInventoryJSON, "utf8", (err) => {
       if (err) {
         console.log(err);
       } else {
         res.send(200);
       }
-    }
-  );
-  console.log(inventories);
-});
+    });
+  });
 
+  // PUT/PATCH/EDIT Inventory Item
+
+  router.put("/:inventoryId", (req, res) => {
+    const inventories = require("../data/inventories.json");
+    const inventoryId = req.params.inventoryId;
+    const matchingInventory = inventories.find(
+      (inventory) => inventory.id === inventoryId
+    );
+
+    if (matchingInventory) {
+      matchingInventory.warehouseName = req.body.warehouseName;
+      matchingInventory.itemName = req.body.itemName;
+      matchingInventory.description = req.body.description;
+      matchingInventory.category = req.body.category;
+      matchingInventory.status = req.body.status;
+      matchingInventory.quantity = req.body.quantity;
+    }
+
+    fs.writeFile(
+      "./data/inventories.json",
+      JSON.stringify(inventories, null, 2),
+      "utf8",
+      (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(200);
+        }
+      }
+    );
+    console.log(inventories);
+  });
+})
 module.exports = router;
